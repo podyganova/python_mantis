c16403c6294ffaeeee83003d01825101a669e0ce
podyganova <albina.yandulova@yandex.ru>
2022-02-20 23:53:58 +0300

    Задание_25: Добавление и удаление проектов(2).

---
+++ fixture/session.py
@@ -0,0 +1,46 @@
+class SessionHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def login(self, username, password):
+        wd = self.app.wd
+        self.app.open_page()
+        wd.find_element_by_name("username").click()
+        wd.find_element_by_name("username").clear()
+        wd.find_element_by_name("username").send_keys(username)
+        wd.find_element_by_name("password").click()
+        wd.find_element_by_name("password").clear()
+        wd.find_element_by_name("password").send_keys(password)
+        wd.find_element_by_css_selector('input[type="submit"]').click()
+
+    def logout(self,):
+        wd = self.app.wd
+        wd.find_element_by_xpath("//a[contains(text(),'Logout')]").click()
+        wd.find_element_by_name("username")
+
+    def ensure_logout(self):
+        wd = self.app.wd
+        if self.is_logged_in():
+            self.logout()
+
+    def is_logged_in(self):
+        wd = self.app.wd
+        return len(wd.find_elements_by_xpath("//a[contains(text(),'Logout')]")) > 0
+
+    def is_logged_in_as(self, username):
+        wd = self.app.wd
+        return self.get_login_user() == username
+
+    def get_login_user(self):
+        wd = self.app.wd
+        return wd.find_element_by_css_selector("td.login-info-left span").text
+
+    def ensure_login(self, username, password):
+        wd = self.app.wd
+        if self.is_logged_in():
+            if self.is_logged_in_as(username):
+                return
+            else:
+                self.logout()
+        self.login(username, password)
